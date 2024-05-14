98dedb8e7b2aac4e868537fddb6efb3b661b042d
noshotbigjuicersbilal151 <bnasi004@ucr.edu>
2024-05-14 13:57:03 -0700

    segmented for mnist

---
+++ data.py
@@ -0,0 +1,56 @@
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+def get_mnist_loaders(batch_size=128, test_batch_size=1000):
+
+    transform_train = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    train_loader = DataLoader(
+        datasets.MNIST(root='.data/mnist', train=True, download=True, transform=transform_train), batch_size=batch_size,
+        shuffle=True, num_workers=1, drop_last=True
+    )
+
+    train_eval_loader = DataLoader(
+        datasets.MNIST(root='.data/mnist', train=True, download=True, transform=transform_test),
+        batch_size=test_batch_size, shuffle=False, num_workers=1, drop_last=True
+    )
+
+    test_loader = DataLoader(
+        datasets.MNIST(root='.data/mnist', train=False, download=True, transform=transform_test),
+        batch_size=test_batch_size, shuffle=False, num_workers=1, drop_last=True
+    )
+
+    return train_loader, test_loader, train_eval_loader
+''' DATASET LOADER for MNIST
+** TASK IS TO LOAD MNIST DATA **
+
+- batch size = number of samples in each batch for the training dataset
+- test_batch_size = number of sample in each batch for testing and training eval datasets
+
+CODE EXPLANATION
+
+transform_train & transform_test:
+    .ToTensor(): converts a PIL image into a PyTorch tensor. Scales image data to a range of 0.0-1.0 by dividing all pixel values by 255.
+        TLDR: normalizes pil image in a pytorch tensor
+    .Compose(): will apply following transformations to the data
+
+DataLoader(): 
+    root = MNIST: Looks for mnist dataset
+    train = True/False: says whether to look at the training portion or testing portion of the dataset
+    download = True: says to download dataset
+    transform: specifies what transformations (defined in transform_train and transform_test)
+    batch_size: number of data points to load in each batch of training. impacts speed and stability. 
+    shuffle: asks the loader to shuffle the order of dataset. allows for training of different batches per epoch. results in a more generalized model
+    num_workers: specifies how many subprocesses to use for data loading. more workers increases throughput and decrese time it takes to load data at the cost of increased memory usage. 
+        TLDR: specifies how many cpus to use for data loading, more = more cpu usage, memory usage.
+    drop_last: drops last incomplete batch so that it doesnt train on a batch thats less than batch_size
+
+    RETURNS three loader configurations
+'''
---
+++ train.py
@@ -0,0 +1,115 @@
+import os
+import argparse
+import logging
+import time
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from model import ODEfunc, ODEBlock, get_mnist_downsampling_layers, get_fc_layers
+from utils import norm, RunningAverageMeter, inf_generator, learning_rate_with_decay, accuracy, makedirs, get_logger, count_parameters
+from data import get_mnist_loaders
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--tol', type=float, default=1e-3)
+parser.add_argument('--adjoint', type=eval, default=False, choices=[True, False])
+parser.add_argument('--downsampling-method', type=str, default='conv', choices=['conv', 'res'])
+parser.add_argument('--nepochs', type=int, default=160)
+parser.add_argument('--lr', type=float, default=0.1)
+parser.add_argument('--batch_size', type=int, default=128)
+parser.add_argument('--test_batch_size', type=int, default=1000)
+parser.add_argument('--save', type=str, default='./experiment1')
+parser.add_argument('--debug', action='store_true')
+parser.add_argument('--gpu', type=int, default=0)
+args = parser.parse_args()
+
+if args.adjoint:
+    from torchdiffeq import odeint_adjoint as odeint
+else:
+    from torchdiffeq import odeint
+
+
+
+if __name__ == '__main__':
+
+    makedirs(args.save)
+    logger = get_logger(logpath=os.path.join(args.save, 'logs'), filepath=os.path.abspath(__file__))
+    logger.info(args)
+
+    #find device
+    device = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 'cpu')
+
+
+    #first section of network, primarily focusing on reducing spatial dimensions of input while extracting basic to complex features
+    downsampling_layers = get_mnist_downsampling_layers()
+    feature_layers = [ODEBlock(ODEfunc(64), odeint, args.tol)]
+    fc_layers = get_fc_layers()
+
+    model = nn.Sequential(*downsampling_layers, *feature_layers, *fc_layers).to(device)
+
+    logger.info(model)
+    logger.info('Number of parameters: {}'.format(count_parameters(model)))
+
+    criterion = nn.CrossEntropyLoss().to(device)
+
+    train_loader, test_loader, train_eval_loader = get_mnist_loaders(args.batch_size, args.test_batch_size)
+
+    data_gen = inf_generator(train_loader)
+    batches_per_epoch = len(train_loader)
+
+    lr_fn = learning_rate_with_decay(
+        args.batch_size, batch_denom=128, batches_per_epoch=batches_per_epoch, boundary_epochs=[60, 100, 140],
+        decay_rates=[1, 0.1, 0.01, 0.001], lr = args.lr
+    )
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
+
+    best_acc = 0
+    batch_time_meter = RunningAverageMeter()
+    f_nfe_meter = RunningAverageMeter()
+    b_nfe_meter = RunningAverageMeter()
+    end = time.time()
+
+    for itr in range(args.nepochs * batches_per_epoch):
+
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr_fn(itr)
+
+        optimizer.zero_grad()
+        x, y = data_gen.__next__()
+        x = x.to(device)
+        y = y.to(device)
+        logits = model(x)
+        loss = criterion(logits, y)
+
+        nfe_forward = feature_layers[0].nfe
+        feature_layers[0].nfe = 0
+
+        loss.backward()
+        optimizer.step()
+
+        nfe_backward = feature_layers[0].nfe
+        feature_layers[0].nfe = 0
+
+        batch_time_meter.update(time.time() - end)
+
+        f_nfe_meter.update(nfe_forward)
+        b_nfe_meter.update(nfe_backward)
+        end = time.time()
+
+        if itr % batches_per_epoch == 0:
+            with torch.no_grad():
+                train_acc = accuracy(model, train_eval_loader, device)
+                val_acc = accuracy(model, test_loader, device)
+                if val_acc > best_acc:
+                    torch.save({'state_dict': model.state_dict(), 'args': args}, os.path.join(args.save, 'model.pth'))
+                    best_acc = val_acc
+                logger.info(
+                    "Epoch {:04d} | Time {:.3f} ({:.3f}) | NFE-F {:.1f} | NFE-B {:.1f} | "
+                    "Train Acc {:.4f} | Test Acc {:.4f}".format(
+                        itr // batches_per_epoch, batch_time_meter.val, batch_time_meter.avg, f_nfe_meter.avg,
+                        b_nfe_meter.avg, train_acc, val_acc
+                    )
+                )
